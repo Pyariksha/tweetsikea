46a7d317e4336f9c175128f9e0f111d627341c5f
Pya Tiluk <50378431+Pyariksha@users.noreply.github.com>
2022-05-02 19:00:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ def bq_load(key, value):
   """
   Function loads data into bq from pandas df.
   """
-  project_name = 'sigma-scheduler-348710'
+  project_name = 'sigma-scheduler-348710'#gcp project name
   dataset_name = 'ikea'
   table_name = key
-  
-  value.to_gbq(destination_table='{}.{}'.format(dataset_name, table_name), project_id=project_name, if_exists='append')
+  value.to_gbq(destination_table='{}.{}'.format(dataset_name, table_name), project_id=project_name, if_exists='append')#append tweets to table
 
 #define function to get tweets to be run in gcp cloud functions
 def get_tweets_ikea(data, context):
@@ -21,7 +20,7 @@ def get_tweets_ikea(data, context):
     This function gets the tweets we want from '#ikea' and saves the 3 required attributes to a pandas dataframe.
     Input params: data and context - default for cloud functions triggered by gcs. param is request for http.
     '''
-    #Bearer Token inserted from gcp functions environment variables for twitter api access
+    #Bearer Token can be inserted from gcp functions environment variables for twitter api access
     client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAADpAcAEAAAAAN9RKdHzdbTki26SRndWSkFNtZY8%3Ddllih5AeitkWwr9NitUkJRdyurMgDcBQeTZpawFOcaQ6EdK7z4')
     data = data
     context = context
@@ -31,11 +30,13 @@ def get_tweets_ikea(data, context):
     query = '#ikea -is:retweet lang:en'
     tweets = client.search_recent_tweets(query=query, tweet_fields=['created_at'], max_results=100)
     try:
-        list = []
+        list = []                       #initialize empty list
         for tweet in tweets.data:
-            list.append(tweet)
-        df = pd.DataFrame(list)
-        bq_load('ikea_table_data', df)
+            list.append(tweet)          #append each tweet to list
+        df = pd.DataFrame(list)         #create dataframe from list
+        df = df.drop_duplicates()       #drop duplicate tweets in dataframe -- good for batcch loads
+        bq_load('ikea_table_data', df)  #write to bigquery table
+        #no need for a return statement as the function output is a load run to BigQueary
     except:
         raise Exception('Error in function get_tweets_ikea.')
         
