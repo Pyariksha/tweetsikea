171d17608f6b4f59ada58c1588e3bdeab9676c65
Pyariksha tiluk <pyar.tiluk@gmail.com>
2022-05-03 02:11:22 +0200

    add pytest

--- main.py
+++ main.py
@@ -14,21 +14,22 @@ def bq_load(key, value):
   table_name = key
   value.to_gbq(destination_table='{}.{}'.format(dataset_name, table_name), project_id=project_name, if_exists='append')#append tweets to table
 
+#Bearer Token can be inserted from gcp functions environment variables for twitter api access
+client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAADpAcAEAAAAAN9RKdHzdbTki26SRndWSkFNtZY8%3Ddllih5AeitkWwr9NitUkJRdyurMgDcBQeTZpawFOcaQ6EdK7z4')
+
+# Get tweets that contain the hashtag #ikea
+# -is:retweet exclude retweets
+# lang:en for the tweets in english
+query = '#ikea -is:retweet lang:en'
+#search tweets based on hashtag - 3 attributes selected (id, created_at, text)
+tweets = client.search_recent_tweets(query=query, tweet_fields=['created_at'], max_results=100)
+
 #define function to get tweets to be run in gcp cloud functions
-def get_tweets_ikea():
+def get_tweets_ikea(tweets):
     '''
     This function gets the tweets we want from '#ikea' and saves the 3 required attributes to a pandas dataframe.
     Input params: data and context - default for cloud functions triggered by gcs. param is request for http.
     '''
-    #Bearer Token can be inserted from gcp functions environment variables for twitter api access
-    client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAADpAcAEAAAAAN9RKdHzdbTki26SRndWSkFNtZY8%3Ddllih5AeitkWwr9NitUkJRdyurMgDcBQeTZpawFOcaQ6EdK7z4')
-
-    # Get tweets that contain the hashtag #ikea
-    # -is:retweet exclude retweets
-    # lang:en for the tweets in english
-    query = '#ikea -is:retweet lang:en'
-    #search tweets based on hashtag - 3 attributes selected (id, created_at, text)
-    tweets = client.search_recent_tweets(query=query, tweet_fields=['created_at'], max_results=1000)
     try:
         list = []                       #initialize empty list
         for tweet in tweets.data:
@@ -37,7 +38,8 @@ def get_tweets_ikea():
         df = df.drop_duplicates()       #drop duplicate tweets in dataframe -- good for batcch loads
         bq_load('ikea_table_data', df)  #write to bigquery table
         #no need for a return statement as the function output is a load run to BigQueary (runs the bq_load function)
+        return df
     except:
         raise Exception('Error in function get_tweets_ikea.') #exception handling to seperate config/run failures from function code errors
         
- get_tweets_ikea() #calls the function as every time gcp runs the function the bq table must update with appended tweets.
+get_tweets_ikea(tweets) #calls the function as every time gcp runs the function the bq table must update with appended tweets.
---
+++ main_test.py
@@ -0,0 +1,12 @@
+import pytest
+import tweepy
+import main
+from main import get_tweets_ikea, bq_load
+
+class TestMain:
+    def test_get_tweets(self):
+        client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAADpAcAEAAAAAN9RKdHzdbTki26SRndWSkFNtZY8%3Ddllih5AeitkWwr9NitUkJRdyurMgDcBQeTZpawFOcaQ6EdK7z4')
+        query = '#ikea -is:retweet lang:en'
+        tweets_test = client.search_recent_tweets(query=query, tweet_fields=['created_at'], max_results=10)
+        x = get_tweets_ikea(tweets_test)
+        assert x.columns.to_list() == ['created_at', 'id', 'text']
